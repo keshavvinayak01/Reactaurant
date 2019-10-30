79a838d6ea25eba009f4100ce1ae91924399c4eb
Keshav Vinayak Jha <keshavvinayakjha@gmail.com>
2019-10-30 12:37:54 +0530

    Revert to cra

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const rootEl = document.getElementById('app');
+ReactDOM.render(<App />, document.getElementById('root'));
 
-render(<App />, rootEl);
-if(module.hot) {
-    module.hot.accept();
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
